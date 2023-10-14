eff9732b0fb284d94e6a12c7d87dee56e6c093ac
Oh <nyoungs117@gmail.com>
2023-10-15 00:01:31 +0900

    li 내용추가

--- js/p2.js
+++ js/p2.js
@@ -7,3 +7,22 @@ $(function(){
 
     });
 });
+
+$(document).ready(function() {
+    // article에서 li 클릭 시 해당 li를 popup-modal에 추가하고 표시
+    $('#select article li').on('click', function() {
+        const selectedText = $(this).find('p').text();
+        const $popupUl = $('#popup-modal ul');
+        const $newLi = $('<li><p>' + selectedText + '</p><img src="../img/x.png" alt="선택 취소 아이콘"></li>');
+        $popupUl.append($newLi);
+        $newLi.show();
+    });
+
+    // popup-modal에서 li의 이미지 클릭 시 해당 li를 삭제
+    $('#popup-modal ul').on('click', 'li', function() {
+        $(this).remove();
+    });
+});
+
+
+
