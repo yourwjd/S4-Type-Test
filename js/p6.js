d0076da3b16b6f51eb4561c1313f670882aaab36
양유정 <yourwjd13@gmail.com>
2023-10-24 12:14:58 +0900

    Update p6.js

--- js/p6.js
+++ js/p6.js
@@ -9,3 +9,24 @@ $(function(){
     });
 
 });
+
+
+// 이벤트 핸들러 함수 등록
+document.addEventListener('DOMContentLoaded', function() {
+    const slider1 = document.getElementById('slider1'); // 단맛 슬라이더
+    const slider2 = document.getElementById('slider2'); // 신맛 슬라이더
+    const slider3 = document.getElementById('slider3'); // 짠맛 슬라이더
+    const slider4 = document.getElementById('slider4'); // 매운맛 슬라이더
+    const resultButton = document.querySelector('#submit-button');
+
+    // 결과 버튼 클릭 이벤트 핸들러
+    resultButton.addEventListener('click', function() {
+        const pValue = parseInt(slider1.value, 10);
+        const yValue = parseInt(slider2.value, 10);
+        const bValue = parseInt(slider3.value, 10);
+        const gValue = parseInt(slider4.value, 10);
+
+        // 결과 페이지로 이동하면서 선택한 값들을 쿼리 매개변수로 전달
+        window.location.href = `./result.html?p=${pValue}&y=${yValue}&b=${bValue}&g=${gValue}`;
+    });
+});
