cbb9321d4c4b78d7b52aadbc513e9ed8708c8de9
양유정 <yourwjd13@gmail.com>
2023-11-03 03:14:13 +0900

    Update p6.js

--- js/p6.js
+++ js/p6.js
@@ -26,26 +26,14 @@ document.addEventListener('DOMContentLoaded', function() {
         const bValue = parseInt(slider3.value, 10);
         const gValue = parseInt(slider4.value, 10);
 
+        // 모든 슬라이더 값이 0인 경우
+        if(pValue === 0 && yValue === 0 && bValue === 0 && gValue === 0) {
+            // 경고 모달창 띄우기
+            alert("모든 값이 0입니다. 다시 설정해주세요.");
+            return;
+        }
+        
         // 결과 페이지로 이동하면서 선택한 값들을 쿼리 매개변수로 전달
         window.location.href = `./result.html?p=${pValue}&y=${yValue}&b=${bValue}&g=${gValue}`;
     });
-});
-
-
-// 결과 버튼 클릭 이벤트 핸들러
-resultButton.addEventListener('click', function() {
-    const pValue = parseInt(slider1.value, 10);
-    const yValue = parseInt(slider2.value, 10);
-    const bValue = parseInt(slider3.value, 10);
-    const gValue = parseInt(slider4.value, 10);
-
-    // 모든 슬라이더 값이 0인 경우
-    if(pValue === 0 && yValue === 0 && bValue === 0 && gValue === 0) {
-        // 경고 모달창 띄우기
-        alert("모든 값이 0입니다. 다시 설정해주세요.");
-        return;
-    }
-
-    // 결과 페이지로 이동하면서 선택한 값들을 쿼리 매개변수로 전달
-    window.location.href = `./result.html?p=${pValue}&y=${yValue}&b=${bValue}&g=${gValue}`;
 });
