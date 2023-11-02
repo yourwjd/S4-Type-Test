03517ae9fbf67965230c37282f657ecb724fbca3
Oh <nyoungs117@gmail.com>
2023-11-03 02:49:28 +0900

    ㅇㅇ

--- js/p2.js
+++ js/p2.js
@@ -112,95 +112,126 @@ $(function(){
 
 
 $(function() {
-    const $popupModal2 = $('#popup-modal2');
-    const $listInner = $('.listInner');
-  
-    function adjustModalHeight() {
-        const $listItems = $popupModal2.find('ul li');
-        $popupModal2.css('height', $listItems.length > 0 ? '210px' : '170px');
-    }
-    
-    function handleListItemRemoval($li) {
-      const selectedText = $li.find('p').text();
-      const $relatedListInner = $listInner.filter(function() {
-        return $(this).find('.foodname').text() === selectedText;
-      });
-  
-      $relatedListInner.find('input[type="checkbox"]').prop('checked', false);
-      $relatedListInner.find('.foodname').css('color', 'white');
-      $relatedListInner.removeClass('active');
-      $li.remove();
-  
-      adjustModalHeight();
-    }
-  
-    $('#open-modal-button').click(function() {
-      $('#modal').addClass('active');
-      $popupModal2.appendTo('#modal').css('height', '170px');
-    });
-  
-    $listInner.click(function() {
-      $(this).toggleClass('active');
-      adjustModalHeight();
-    });
-  
-    $('#close-button, .listInner input[type="checkbox"], .listInner .foodname').click(function() {
-      const $listInnerItem = $(this).closest('.listInner');
-      const checkbox = $listInnerItem.find('input[type="checkbox"]');
-      checkbox.prop('checked', !checkbox.prop('checked'));
-      $(this).css('color', checkbox.prop('checked') ? '#00DB4A' : 'white');
-  
-      if (checkbox.prop('checked')) {
-        $listInnerItem.addClass('active');
-        const foodnameText = $(this).text();
-        const $newPM2Li = $('<li class="p-m2-li"><p>' + foodnameText + '</p><img src="../img/x.png" alt="선택 취소 아이콘"></li>');
-        $newPM2Li.click(function() {
-          handleListItemRemoval($(this));
-        });
-        $popupModal2.find('ul').append($newPM2Li);
-      } else {
-        $listInnerItem.removeClass('active');
-        const foodnameText = $(this).text();
-        const $selectedPM2Li = $popupModal2.find('ul').find('li').filter(function() {
-          return $(this).find('p').text() === foodnameText;
-        });
-        $selectedPM2Li.remove();
-      }
-      adjustModalHeight();
-    });
-  
-    $popupModal2.on('click', 'li img', function() {
-      handleListItemRemoval($(this).closest('li'));
-      adjustModalHeight();
-    });
-  
-    $('#close-button').click(function() {
-      $('#modal').removeClass('active');
-      $popupModal2.css('height', '170px');
-    });
-  });
-  
-  function filter() {
-    let search = document.getElementById("search").value.toLowerCase();
-    let listInner = document.getElementsByClassName("listInner");
-  
-    let hiddenCount = 0;
-    for (let i = 0; i < listInner.length; i++) {
-      foodname = listInner[i].getElementsByClassName("foodname");
-      if (foodname[0].innerHTML.toLowerCase().indexOf(search) != -1) {
-        listInner[i].style.display = "flex";
-      } else {
-        listInner[i].style.display = "none";
-        hiddenCount++;
-      }
-    }
-  
-    const $popupModal2 = $('#popup-modal2');
+  const $popupModal2 = $('#popup-modal2');
+  const $listInner = $('.listInner');
+
+  function adjustModalHeight() {
     const $listItems = $popupModal2.find('ul li');
-    
-    if ($listItems.length === 0) {
-      $popupModal2.css('height', '170px');
+    $popupModal2.css('height', $listItems.length > 0 ? '210px' : '170px');
+  }
+
+  function handleListItemAddition($listInnerItem) {
+    const foodnameText = $listInnerItem.find('.foodname').text();
+    const $newPM2Li = $('<li class="p-m2-li"><p>' + foodnameText + '</p><img src="../img/x.png" alt="선택 취소 아이콘"></li>');
+    $newPM2Li.click(function() {
+      handleListItemRemoval($(this));
+    });
+    $popupModal2.find('ul').append($newPM2Li);
+  }
+
+  function handleListItemRemoval($li) {
+    const selectedText = $li.find('p').text();
+    const $relatedListInner = $listInner.filter(function() {
+      return $(this).find('.foodname').text() === selectedText;
+    });
+
+    $relatedListInner.find('input[type="checkbox"]').prop('checked', false);
+    $relatedListInner.find('.foodname').css('color', 'white');
+    $relatedListInner.removeClass('active');
+    $li.remove();
+
+    adjustModalHeight();
+  }
+
+  $('#open-modal-button').click(function() {
+    $('#modal').addClass('active');
+    $popupModal2.appendTo('#modal').css('height', '170px');
+  });
+
+  $listInner.find('input[type="checkbox"]').click(function(event) {
+    event.stopPropagation();
+
+    const $listInnerItem = $(this).closest('.listInner');
+    const checkbox = $(this);
+    const currentChecked = checkbox.prop('checked');
+    checkbox.prop('checked', !currentChecked);
+    $listInnerItem.find('.foodname').css('color', !currentChecked ? '#00DB4A' : 'white');
+
+    const foodnameText = $listInnerItem.find('.foodname').text();
+    const $selectedPM2Li = $popupModal2.find('ul').find('li').filter(function() {
+        return $(this).find('p').text() === foodnameText;
+    });
+
+    if (!currentChecked) {
+      if ($selectedPM2Li.length === 0) {
+        // 'li'가 존재하지 않을 때만 추가
+        $listInnerItem.addClass('active');
+        handleListItemAddition($listInnerItem);
+      }
     } else {
-      $popupModal2.css('height', '210px');
+      $listInnerItem.removeClass('active');
+      handleListItemRemoval($selectedPM2Li);
+    }
+    adjustModalHeight();
+  });
+
+
+  $listInner.find('input[type="checkbox"], .foodname').click(function(event) {
+    event.stopPropagation();
+
+    const $listInnerItem = $(this).closest('.listInner');
+    const checkbox = $listInnerItem.find('input[type="checkbox"]');
+    const currentChecked = checkbox.prop('checked');
+    checkbox.prop('checked', !currentChecked);
+    $listInnerItem.find('.foodname').css('color', !currentChecked ? '#00DB4A' : 'white');
+
+    if (!currentChecked) {
+      $listInnerItem.addClass('active');
+      handleListItemAddition($listInnerItem);
+    } else {
+      $listInnerItem.removeClass('active');
+      const foodnameText = $listInnerItem.find('.foodname').text();
+      const $selectedPM2Li = $popupModal2.find('ul').find('li').filter(function() {
+        return $(this).find('p').text() === foodnameText;
+      });
+      handleListItemRemoval($selectedPM2Li);
+    }
+    adjustModalHeight();
+  });
+
+  $popupModal2.on('click', 'li img', function() {
+    handleListItemRemoval($(this).closest('li'));
+    adjustModalHeight();
+  });
+
+  $('#close-button').click(function() {
+    $('#modal').removeClass('active');
+    $popupModal2.css('height', '170px');
+  });
+});
+
+function filter() {
+  let search = document.getElementById("search").value.toLowerCase();
+  let listInner = document.getElementsByClassName("listInner");
+
+  let hiddenCount = 0;
+  for (let i = 0; i < listInner.length; i++) {
+    foodname = listInner[i].getElementsByClassName("foodname");
+    if (foodname[0].innerHTML.toLowerCase().indexOf(search) != -1) {
+      listInner[i].style.display = "flex";
+    } else {
+      listInner[i].style.display = "none";
+      hiddenCount++;
     }
   }
+
+  const $popupModal2 = $('#popup-modal2');
+  const $listItems = $popupModal2.find('ul li');
+  const $listInnerItems = $listInner.filter('.active');
+  
+  if ($listItems.length === 0 && $listInnerItems.length === 0) {
+    $popupModal2.css('height', '170px');
+  } else {
+    $popupModal2.css('height', '210px');
+  }
+}
